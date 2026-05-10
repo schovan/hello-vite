48d2748b37fa404ee2e9cfa9e20575d2a36ea1bd
schovan <schovan@gmail.com>
2026-05-10 14:28:08 +0200

    added tauri windows build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
+const isTauri = process.env.TAURI_ENV_PLATFORM !== undefined;
+
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), ...(!isTauri ? [cloudflare()] : [])],
 });
