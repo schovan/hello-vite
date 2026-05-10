f9ad03731d447c28d61b46b0149989f55ef6672c
schovan <schovan@gmail.com>
2026-05-10 22:30:24 +0200

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
-const isTauri = process.env.TAURI_ENV_PLATFORM !== undefined;
-
-export default defineConfig({
-  plugins: [react(), ...(!isTauri ? [cloudflare()] : [])],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "TAURI_ENV");
+  const isTauri = env.TAURI_ENV_PLATFORM !== undefined;
+  return {
+    plugins: [react(), ...(!isTauri ? [cloudflare()] : [])],
+  };
 });
